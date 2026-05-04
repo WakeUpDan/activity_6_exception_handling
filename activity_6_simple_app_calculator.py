ed2c53ff65ea913f7a7ab46c36091cbe7fcda02d
Dan <dllaneza552@gmail.com>
2026-05-04 19:20:16 +0800

    Added a adder class implementing Math Operation inheritance

--- activity_6_simple_app_calculator.py
+++ activity_6_simple_app_calculator.py
@@ -4,3 +4,6 @@ class MathOperation:
     """Base class for all math logic."""
     def execute(self, a: float, b: float) -> float:
         raise NotImplementedError("Each operation must define its own execute method.")
+    
+    class Adder(MathOperation):
+     def execute(self, a, b): return a + b
